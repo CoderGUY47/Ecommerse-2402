c29c8553b4a206105de5e6f79917767389b6db7f
CoderGUY47 <shkhridoy92@gmail.com>
2025-02-17 01:08:30 +0600

    cart full done

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const CartList = ({carthead, cartdata,price, quantity, total, className, text}) => {
+const CartList = ({carthead, cartdata,price, quantity, total, subdollar, totaldollar, className}) => {
   return (
     <div>
-      <table>
-        <h5 className={`font-dm font-semibold text-base text-primary ${className, text}`}>{carthead}</h5>
-        <p className={`font-dm font-semibold text-base text-primary ${className, text}`}>{cartdata}</p>
-        <p className={`font-dm font-bold text-[20px] text-primary ${className, text}`}>{price}</p>
-        <p className={`font-dm font-semibold text-base text-[#767676] ${className, text}`}>{quantity}</p>
-        <p className={`font-dm font-bold text-[20px] text-primary ${className, text}`}>{total}</p>
-      </table>
+        <h5 className={`font-dm font-semibold text-base text-primary ${className}`}>{carthead}</h5>
+        <p className={`font-dm font-semibold text-base text-primary ${className}`}>{cartdata}</p>
+        <p className={`font-dm font-bold text-[20px] text-primary ${className}`}>{price}</p>
+        <p className={`font-dm font-semibold text-base text-[#767676] ${className}`}>{quantity}</p>
+        <p className={`font-dm font-bold text-[20px] text-primary ${className}`}>{total}</p> 
+        <p className={`font-dm font-normal text-base text-[#767676] ${className}`}>{subdollar}</p> 
+        <p className={`font-dm font-medium text-base text-primary ${className}`}>{totaldollar}</p> 
     </div>
   )
 }
